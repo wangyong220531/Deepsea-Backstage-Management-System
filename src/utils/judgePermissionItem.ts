d54ad3ffb080d6ccf92b4e1bc192e32c9d5fe30f
wang yong <1912573974@qq.com>
2023-05-05 13:31:49 +0800

    update

--- src/utils/judgePermissionItem.ts
+++ src/utils/judgePermissionItem.ts
@@ -1,6 +1,7 @@
 const judgePermissionItem = (data: PermissionResult[]) => {
+    const result: string[] = []
     if (data.find(e => e.name === "首页")) {
-        return "0"
+        result.push("0")
     }
     const followChidren: PermissionResult[] | undefined = structuredClone(
         data
@@ -8,5 +9,24 @@ const judgePermissionItem = (data: PermissionResult[]) => {
             ?.children?.find(e => e.name === "警情处置")
             ?.children?.find(e => e.name === "跟进处置")?.children
     )
-    followChidren && console.log(1)
+    if (followChidren) {
+        if (followChidren.find(e => e.name === "警力跟进")) {
+            const forceFollow = structuredClone(followChidren.find(e => e.name === "警力跟进"))
+            if (forceFollow?.children?.find(e => e.name === "跟进")) {
+                result.push("1-0-0")
+            }
+            if (forceFollow?.children?.find(e => e.name === "处置")) {
+                result.push("1-0-1")
+            }
+            if (forceFollow?.children?.find(e => e.name === "到场")) {
+                result.push("1-0-2")
+            }
+        }
+        if (followChidren.find(e => e.name === "警情跟进")) {
+            const forceFollow = structuredClone(followChidren.find(e => e.name === "警情跟进"))
+            if (forceFollow?.children?.find(e => e.name === "跟进")) {
+                result.push("1-1-0")
+            }
+        }
+    }
 }
